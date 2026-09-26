a1aad317262552109b509e5449f09d3abcf0d228
Peter Prieboj <prieboj.peter@gmail.com>
2026-09-26 15:25:54 +0200

    Design manual

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,1 +1,8 @@
-<flux:icon name="chef-hat" {{ $attributes }} />
+{{-- Znak „Moje recepty“: kuchárska čiapka (silueta), farba podľa currentColor. Zdroj: docs/brand/logo/mark-*.svg --}}
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" viewBox="11 11 42 42" fill="currentColor" aria-hidden="true">
+    <circle cx="32" cy="23.2" r="12.1"/>
+    <circle cx="19.35" cy="29.25" r="9.35"/>
+    <circle cx="44.65" cy="29.25" r="9.35"/>
+    <rect x="13.3" y="29.25" width="37.4" height="12.1"/>
+    <rect x="13.3" y="44.65" width="37.4" height="8.25" rx="2.75"/>
+</svg>
